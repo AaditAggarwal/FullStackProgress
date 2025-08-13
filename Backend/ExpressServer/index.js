201e62b769022e6bbdd319d002715a42359eeff8
AaditAggarwal <aadit.asr5904@gmail.com>
2025-08-13 16:09:46 +0530

    HTTP requests

--- Backend/ExpressServer/index.js
+++ Backend/ExpressServer/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Youkoso!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Ore wa Aaditto desu!</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>P:945-900-XXXX</h1>");
 });
 
 app.listen(port, () => {
